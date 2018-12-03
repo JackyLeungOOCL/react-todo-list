5193fa53ff8a412f8924c9cc8ce1ab99db2cdf1e
jacky.leung <jacky.leung@oocl.com>
2018-12-03 23:43:12 +0800

    [LEUNGJA] feat: add user input to todo list

--- src/App.js
+++ src/App.js
@@ -9,15 +9,9 @@ class App extends Component {
     todoList : []
   }
 
-  addTodo = (task) => {
-    this.state.todoList.push(task);
-    console.log(this.state.todoList);
-  }
-
   render() {
     return (
       <div className="App">
-        <UserInput onUpdate={this.addTodo} />
         <TodoList todoList={this.state.todoList} />
       </div>
     );
--- src/Components/TodoList.js
+++ src/Components/TodoList.js
@@ -1,19 +1,30 @@
 import React, { Component } from 'react';
+import UserInput from './UserInput';
 import Todo from './Todo';
 
 export default class TodoList extends Component {
   state = {
-    todoList : new Array(1).fill(0)
+    todoList : []
   };
+
+  addTodo = (task) => {
+    if (task != "") {
+      this.state.todoList.push(task);
+      this.setState({
+        todoList : this.state.todoList
+      })
+    }
+  }
 
   render() {
     return (
       <div>
+        <UserInput onUpdate={this.addTodo} />
         <h2>Todos</h2>
         <ul>
           {this.state.todoList.map((todo) => <Todo task={todo}/>)}
         </ul>
       </div>
-    )
+    );
   }
 }
