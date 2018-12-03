31b7803e2a756bb8cde4b365cf0a2ddbc61eb4f3
jacky.leung <jacky.leung@oocl.com>
2018-12-03 22:42:40 +0800

    [LEUNGJA] feat: add user input view

---
+++ src/Components/UserInput.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class UserInput extends Component {
+  render() {
+    return (
+      <div>
+        <input></input>
+        <button>add todo</button>
+      </div>
+    )
+  }
+}
